948335660d4673667ecaef0954a97e0df10dc19f
Your Name <you@example.com>
2026-05-17 03:27:41 +0300

    Bump app version to 1.0.5

--- appVersion.py
+++ appVersion.py
@@ -5,8 +5,8 @@
 # ##########################################################
 
 APP_NAME = "FlatCAM Plus"
-APP_VERSION = "1.0.4"
-APP_VERSION_DATE = "2026.05.14"
+APP_VERSION = "1.0.5"
+APP_VERSION_DATE = "2026.05.17"
 APP_RELEASE_CHANNEL = "beta"
 APP_BETA = True
 
